d1e443729b54f77904c94a5fcb71c32d840c7dcf
Tomasz Kuehn <tomasz.kuehn@gmail.com>
2020-06-10 01:18:00 +0200

    initial test

--- M5Stack.XYimage.ino
+++ M5Stack.XYimage.ino
@@ -1,9 +1,29 @@
+#include <driver/dac.h>
+
 void setup() {
-  // put your setup code here, to run once:
+  dac_output_enable(DAC_CHANNEL_1);
+  dac_output_enable(DAC_CHANNEL_2);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int col, line;
+  for(line = 0; line < 100; line++) {
+    dac_output_voltage(DAC_CHANNEL_1, line * 2);
+    for(col = 0; col < 100; col++) {
+      if(col % 3 == 0) {
+        dac_output_voltage(DAC_CHANNEL_1, line * 2);
+        dac_output_voltage(DAC_CHANNEL_2, col * 2);
+      }
+      else
+      {
+        dac_output_voltage(DAC_CHANNEL_1, 0);
+        dac_output_voltage(DAC_CHANNEL_2, col * 2);
+      }
+   
+    }
+  }
+  dac_output_voltage(DAC_CHANNEL_1, 254);
+  dac_output_voltage(DAC_CHANNEL_2, 254);
+  delay(20);
 }
